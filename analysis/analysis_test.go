bbecee7bca7e8f990a26b28060e4828231c437ba
kenshi kamata <kenshi.kamata@gmail.com>
2019-03-13 01:37:35 +0900

    fix test

--- analysis/analysis_test.go
+++ analysis/analysis_test.go
@@ -1,13 +1,12 @@
-package analysis_test
+package analysis
 
 import (
 	"testing"
 
-	"github.com/knsh14/go-conditional-complexity/analysis"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func Test(t *testing.T) {
 	testdata := analysistest.TestData()
-	analysistest.Run(t, testdata, analysis.Analyzer, "complexity")
+	analysistest.Run(t, testdata, Analyzer, "complexity")
 }
